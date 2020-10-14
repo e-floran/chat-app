d6ce7b88633295a5c8b44be3ea1294c509e4007d
e-floran <e-floran@hotmail.fr>
2020-10-14 10:44:21 +0200

    changed for react 5

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,19 +2,31 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import "./Contact.css";
 
-function Contact(props){
-    return (
-        <article className="Contact">
-            <img className="avatar" src={props.avatar} alt={props.name}></img>
-            <div>
-                <h4 className="name">{props.name}</h4>
-                <div className="status">
-                    <div className={props.online ? 'status-online' : 'status-offline'}></div>
-                    <p className="status-text">{props.online ? 'Online' : 'Offline'}</p>
+class Contact extends React.Component {
+    constructor(props){
+        super(props);
+        this.state ={
+            online : this.props.online
+        };
+    }
+    render(){
+        return (
+            <article className="Contact">
+                <img className="avatar" src={this.props.avatar} alt={this.props.name}></img>
+                <div>
+                    <h4 className="name">{this.props.name}</h4>
+                    <div className="status" onClick={event => {
+                        const contrary = !(this.state.online);
+                        this.setState({online : contrary});
+                        }}
+                    >
+                        <div className={this.state.online ? 'status-online' : 'status-offline'}></div>
+                        <p className="status-text">{this.state.online ? 'Online' : 'Offline'}</p>
+                    </div>
                 </div>
-            </div>
-        </article>
-    )
+            </article>
+        )
+    }
 }
 Contact.propTypes = {
     avatar: PropTypes.string.isRequired,
